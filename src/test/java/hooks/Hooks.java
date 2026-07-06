17d743be677c026421946c438dcf8430ae340cd0
Kamlesh Vaghela QA <168186840+KamleshVaghelaQA@users.noreply.github.com>
2026-07-06 17:17:43 +0530

    New Changes in POM
    Changes in hook, runnner , pom

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,33 +1,29 @@
 package hooks;
 
-import com.aventstack.extentreports.ExtentTest;
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import utils.DriverFactory;
-import utils.ExtentManager;
 
 public class Hooks {
-
-    public static ExtentTest test;
 
     @Before
     public void setup(Scenario scenario) {
         DriverFactory.initDriver();
-        test = ExtentManager.getInstance().createTest(scenario.getName());
+        System.out.println("Starting Scenario: " + scenario.getName());
     }
 
     @After
     public void tearDown(Scenario scenario) {
 
         if (scenario.isFailed()) {
-            test.fail("Test Failed");
+            System.out.println("Scenario Failed: " + scenario.getName());
         } else {
-            test.pass("Test Passed");
+            System.out.println("Scenario Passed: " + scenario.getName());
         }
 
-        ExtentManager.getInstance().flush();
-        DriverFactory.getDriver().quit();
+        if (DriverFactory.getDriver() != null) {
+            DriverFactory.getDriver().quit();
+        }
     }
 }
